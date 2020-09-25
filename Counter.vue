8fd2b0fe535390482cd819d7b3ff156c8e090cbf
jerryliangxia <60997074+jerryliangxia@users.noreply.github.com>
2020-09-25 00:01:26 -0300

    Add files via upload

---
+++ Counter.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+  <p>I'm a Counter!</p>
+  <button v-on:click="count++"> {{ count }} times.</button>
+  <button v-on:click="count--"> {{ count }} times.</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    };
+  },
+
+};
+</script>
+
+<style>
+</style>
+
